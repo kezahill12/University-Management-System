518d20cc6367cf956fdf67dabbcd0126225ccf7e
kezahill12 <kezahill12@gmail.com>
2026-02-25 07:48:42 +0200

    added menu of Createing course

--- src/universitymanagement/Main.java
+++ src/universitymanagement/Main.java
@@ -24,6 +24,7 @@ public class Main {
 
         System.out.println();
 
+        System.out.println("=== Create Course ===");
                  manager.createCourse(course1);
 
         System.out.println();
